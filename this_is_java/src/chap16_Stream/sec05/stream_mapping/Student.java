fdadcc67f076189daca09b01a149f3f6cb346d44
yskim621 <yskim62100@gmail.com>
2021-05-03 10:56:26 +0900

    21.05.03 Studying sorting and mapping in Stream

---
+++ this_is_java/src/chap16_Stream/sec05/stream_mapping/Student.java
@@ -0,0 +1,26 @@
+package chap16_Stream.sec05.stream_mapping;
+
+public class Student {
+	private String name;
+	private int score;
+	
+	
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public int getScore() {
+		return score;
+	}
+	
+	
+	
+}
