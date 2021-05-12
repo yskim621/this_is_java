12415dd1a7d7a6416db97e001f36032d0acfceb4
yskim621 <yskim62100@gmail.com>
2021-05-13 00:01:15 +0900

    21.05.13 studying JavaFX with Scenebuilder

--- this_is_java/src/chap17_javaFX/sec04/exam02_hbox_vbox/AppMain.java
+++ this_is_java/src/chap17_javaFX/sec04/exam02_hbox_vbox/AppMain.java
@@ -1,22 +1,20 @@
-package chap17_javaFX.sec04.exam01_anchorpane;
-
-import java.io.IOException;
+package chap17_javaFX.sec04.exam02_hbox_vbox;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
 	@Override
 	// UI 부분은 fxml 파일로 대체
 	public void start(Stage primaryStage) throws Exception  {
-		Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
+		VBox root = (VBox)FXMLLoader.load(getClass().getResource("Root.fxml"));
 		Scene scene = new Scene(root);
-		
+			
 		primaryStage.setTitle("AppMain");
-		primaryStage.setScene(scene);
+		primaryStage.setScene(scene);			
 		primaryStage.show();
 	}
 	
