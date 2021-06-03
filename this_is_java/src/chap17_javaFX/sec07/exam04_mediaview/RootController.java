354b451cf761aee2bbf1eff75dda83d61bdd3afc
yskim621 <yskim62100@gmail.com>
2021-06-04 00:04:11 +0900

    21.06.04 Studying MediaView in Javafx

--- this_is_java/src/chap17_javaFX/sec07/exam04_mediaview/RootController.java
+++ this_is_java/src/chap17_javaFX/sec07/exam04_mediaview/RootController.java
@@ -3,86 +3,90 @@ package chap17_javaFX.sec07.exam04_mediaview;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class RootController implements Initializable {
-	@FXML private ListView<String> listView;
-	@FXML private TableView<Phone> tableView;
+	@FXML private MediaView mediaView;
 	@FXML private ImageView imageView;
+	@FXML private Button btnPlay;
+	@FXML private Button btnPause;
+	@FXML private Button btnStop;
 	
+	private boolean endOfMedia;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		listView.setItems(FXCollections.observableArrayList(
-			"갤럭시S1","갤럭시S2","갤럭시S3","갤럭시S4","갤럭시S5","갤럭시S6","갤럭시S7" 
-		));
+		//Media media = new Media(getClass().getResource("media/video.m4v").toString());
+		Media media = new Media(getClass().getResource("media/audio.wav").toString());
 		
-		listView.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>(){
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaView.setMediaPlayer(mediaPlayer);
+		
+		mediaPlayer.setOnReady(new Runnable() {
 
 			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				tableView.getSelectionModel().select(newValue.intValue());
-				tableView.scrollTo(newValue.intValue());
+			public void run() {
+				btnPlay.setDisable(false);
+				btnPause.setDisable(true);
+				btnStop.setDisable(true);
+				// 미디어 플레이어가 오디오를 플레이하면
+				if(mediaPlayer.isAutoPlay()) {
+					mediaView.setVisible(false);
+				}
 			}
 			
 		});
 		
-		ObservableList phoneList = FXCollections.observableArrayList(
-				new Phone("갤럭시S1", "phone01.png"),
-				new Phone("갤럭시S2", "phone02.png"),
-				new Phone("갤럭시S3", "phone03.png"),
-				new Phone("갤럭시S4", "phone04.png"),
-				new Phone("갤럭시S5", "phone05.png"),
-				new Phone("갤럭시S6", "phone06.png"),
-				new Phone("갤럭시S7", "phone07.png")
-		);
-		
-		TableColumn tcSmartPhone = tableView.getColumns().get(0);
-		tcSmartPhone.setCellValueFactory(new PropertyValueFactory("smartPhone"));
-		tcSmartPhone.setStyle("-fx-alignment: CENTER;");
-		
-		TableColumn tcImage = tableView.getColumns().get(1);
-		tcImage.setCellValueFactory(new PropertyValueFactory("image"));
-		tcImage.setStyle("-fx-alignment: CENTER;");
-		
-		tableView.setItems(phoneList);
-		
-		tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Phone>() {
-
-			@Override
-			public void changed(ObservableValue<? extends Phone> observable, Phone oldValue, Phone newValue) {
-				
-				if(newValue != null) // 선택 전/후로 change method가 호출되므로 선택 해제 시 null 값이 들어감 
-				imageView.setImage(new Image(getClass().getResource("images/" + newValue.getImage()).toString()));
-			}
-				
+		mediaPlayer.setOnPlaying(()->{
+			btnPlay.setDisable(true);
+			btnPause.setDisable(false);
+			btnStop.setDisable(false);
 		});
-	}
-	
-	public void handleBtnOkAction(ActionEvent e) {
-		String item = listView.getSelectionModel().getSelectedItem();
-		System.out.println("ListView 스마트폰: " + item);
 		
-		Phone phone = tableView.getSelectionModel().getSelectedItem();
-		System.out.println("TableView 스마트폰: " + phone);
-		System.out.println("TableView 이미지: " + phone);
+		mediaPlayer.setOnPaused(()->{
+			btnPlay.setDisable(false);
+			btnPause.setDisable(true);
+			btnStop.setDisable(true);
+		});
+		
+		mediaPlayer.setOnStopped(()->{
+			btnPlay.setDisable(false);
+			btnPause.setDisable(true);
+			btnStop.setDisable(true);
+		});
+		
+		mediaPlayer.setOnEndOfMedia(()->{
+			endOfMedia = true;
+			btnPlay.setDisable(false);
+			btnPause.setDisable(true);
+			btnStop.setDisable(true);
+		});
+		
+		btnPlay.setOnAction(event->{
+			if(endOfMedia) {
+				mediaPlayer.stop();
+				mediaPlayer.seek(mediaPlayer.getStartTime());
+			}
+			mediaPlayer.play();
+			endOfMedia = false;
+		});
+		
+		btnPause.setOnAction(event->{
+			mediaPlayer.pause();
+		});
+		
+		btnStop.setOnAction(event->{
+			mediaPlayer.stop();
+		});
+		
 	}
 	
-	public void handleBtnCancelAction(ActionEvent e) {
-		Platform.exit();
-	}
+	
 
 }
