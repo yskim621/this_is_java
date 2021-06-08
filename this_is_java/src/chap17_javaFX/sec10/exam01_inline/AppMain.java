b638d48ea3a8c42e8f702d393008d0a461af54f0
yskim621 <yskim62100@gmail.com>
2021-06-09 00:02:09 +0900

    21.06.09 Studying CSS of inline in Javafx

---
+++ this_is_java/src/chap17_javaFX/sec10/exam01_inline/AppMain.java
@@ -0,0 +1,26 @@
+package chap17_javaFX.sec10.exam01_inline;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+	@Override
+	// UI 부분은 fxml 파일로 대체
+	public void start(Stage primaryStage) throws Exception  {
+		Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
+
