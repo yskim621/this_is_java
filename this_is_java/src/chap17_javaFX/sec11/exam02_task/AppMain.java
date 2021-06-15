d6dd61e35a5760feb66655fa172aae2582193665
yskim621 <yskim62100@gmail.com>
2021-06-16 00:02:00 +0900

    21.06.16 Studying task of thread in javafx

---
+++ this_is_java/src/chap17_javaFX/sec11/exam02_task/AppMain.java
@@ -0,0 +1,26 @@
+package chap17_javaFX.sec11.exam02_task;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+	@Override
+	// UI 부분은 fxml 파일로 대체
+	public void start(Stage primaryStage) throws Exception  {
+		Parent parent = (Parent)FXMLLoader.load(getClass().getResource("Root.fxml"));
+		Scene scene = new Scene(parent);
+		
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
+
