4a5a1e619de64d94e4328be03f647902658e6455
yskim621 <yskim62100@gmail.com>
2021-07-10 00:03:04 +0900

    21.07.10 studying

---
+++ this_is_java/src/chap18_io/sec05/exam08_objectinputstream_objectoutputstream/ClassC.java
@@ -0,0 +1,10 @@
+package chap18_io.sec05.exam08_objectinputstream_objectoutputstream;
+
+import java.io.Serializable;
+
+public class ClassC implements Serializable {
+	private static final long serialVersionUID =  -123124234563123123L;
+	int field1;
+	int field2;
+	int field3;
+}
