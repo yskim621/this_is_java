d82d9a0473388223b53466f4436b0101f46691be
yskim621 <yskim62100@gmail.com>
2021-07-13 00:21:55 +0900

    21.07.13 Studying filein/outputstream

--- this_is_java/src/chap18_io/sec04/exam01_file/FileExample.java
+++ this_is_java/src/chap18_io/sec04/exam01_file/FileExample.java
@@ -2,7 +2,8 @@ package chap18_io.sec04.exam01_file;
 
 import java.io.File;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileExample {
 
@@ -13,6 +14,24 @@ public class FileExample {
 		File file3 = new File(new URI("file:///Users/yoonsukkim/temp/file3.txt"));
 		
 		if(dir.exists() == false) { dir.mkdir(); }
+		if(file1.exists() == false) { file1.createNewFile(); }
+		if(file2.exists() == false) { file2.createNewFile(); }
+		if(file3.exists() == false) { file3.createNewFile(); }
+		
+		File temp = new File("/Users/yoonsukkim/temp");
+		File[] contents = temp.listFiles();
+		System.out.println("날짜          시간                 형태            크기     이름  ");
+		System.out.println("---------------------------------------------------------------------------");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a HH:mm");
+		for(File file : contents) {
+			System.out.print(new Date(file.lastModified()));
+			if(file.isDirectory()) {
+				System.out.println("\t<Dir>\t\t\t" + file.getName());
+			} else {
+				System.out.println("\t\t\t" + file.length() + "\t" + file.getName());
+			}
+			System.out.println();
+		}
 	}
 
 }
